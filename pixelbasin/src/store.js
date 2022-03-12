cf16825fad0139296714910437c817c41f05e755
Louis Kane <louiskane@Louiss-MBP.lan>
2022-03-11 19:43:43 -0800

    adding dependencies and folder structure

---
+++ pixelbasin/src/store.js
@@ -0,0 +1,22 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index_reducer';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+store.subscribe(() => {
+    // keep track of the previous and current state to compare changes
+    //let previousState = currentState;
+    //currentState = store.getState();
+  });
+  
+  export default store;
